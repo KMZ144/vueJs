236cfd592fe0d75030dc0bf04d2c18715b68c379
Kareem_Mostafa <kareemmostafa114@gmail.com>
2023-05-06 22:27:30 +0300

    first

---
+++ lab01/books.js
@@ -0,0 +1,44 @@
+const books = [
+  {
+    id:1,
+    name:"Eloquent JavaScript, Third Edition",
+    category:"Java Script",
+    author:"Marijn Haverbeke",
+    pages:500,
+    price:300,
+    image:'https://cdn.pixabay.com/photo/2018/01/17/18/43/book-3088775_960_720.jpg'
+  },
+  {
+    id:2,
+    name:"Eloquent JavaScript, Third Edition",
+    category:"Java Script",
+    author:"Marijn Haverbeke",
+    pages:60,
+    price:300,
+    image:'https://cdn.pixabay.com/photo/2016/11/29/07/22/bible-1868070_960_720.jpg'
+  },{
+    id:3,
+    name:"Eloquent JavaScript, Third Edition",
+    category:"Java Script",
+    author:"Marijn Haverbeke",
+    pages:80,
+    price:800,
+    image:'https://cdn.pixabay.com/photo/2016/11/29/07/21/book-1868068_960_720.jpg'
+  },{
+    id:4,
+    name:"Eloquent JavaScript, Third Edition",
+    category:"Java Script",
+    author:"Marijn Haverbeke",
+    pages:30,
+    price:250,
+    image:'https://cdn.pixabay.com/photo/2018/01/17/18/43/book-3088775_960_720.jpg'
+  },{
+    id:5,
+    name:"Eloquent JavaScript, Third Edition",
+    category:"Java Script",
+    author:"Marijn Haverbeke",
+    pages:20,
+    price:100,
+    image:'https://cdn.pixabay.com/photo/2018/01/17/18/43/book-3088775_960_720.jpg'
+  }
+]
